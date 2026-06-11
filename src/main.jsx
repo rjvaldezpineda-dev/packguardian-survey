c20ecb5ecc039a25b9a9d3b68e071c904b9aa563
rjvaldezpineda-dev <rjvaldezpineda@gmail.com>
2026-06-11 07:45:11 -0700

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Survey from './Survey.jsx'
+import Survey from './survey.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(<Survey />)
